9c093a7ed5e4de89be65ec010c5913271ffe91a7
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-10-27 21:12:49 +0000

    3.3. Разбор первой программы. [#327586]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+    System.out.println("Eremenko Liubov");
+    System.out.println("20.10.2020");
+
+
+    }
+}
