212606e100370188ec43125e60c00fac1450753c
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-10-30 18:13:30 +0000

    9. Площадь треугольника. [#327572] настройка CheckStyle

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -3,13 +3,14 @@ package ru.job4j.condition;
 import java.net.PortUnreachableException;
 
 public class TrgArea {
-    public static double area(double a, double b, double c){
-        double p = (a + b + c)/2;
-        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
-
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
-    public static void main(String[] args) {
-        double result = TrgArea.area(2,2,2);
+
+    public static void main(String[] args)    {
+
+        double result = TrgArea.area(2, 2, 2);
         System.out.println("The area of a triangle with sides a=2, b=2, c=2 is " + result);
     }
 }
