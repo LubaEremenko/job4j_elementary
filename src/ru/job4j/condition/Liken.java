18cdbc7e8ec1f0d37e75cf1940101e7c432c8dea
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-11-02 07:43:08 +0000

    0. Операции сравнения [#327526]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 2;
+        int second = 15;
+        boolean compare = first > second;
+        System.out.println(compare);
+
+        boolean compare1 = first < second;
+        System.out.println(compare1);
+
+        boolean compare2 = first == second;
+        System.out.println(compare2);
+
+        }
+
+}
