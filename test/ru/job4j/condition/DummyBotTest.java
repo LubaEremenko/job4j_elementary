576d9260ac4e0ef64528f77e30e6961dbee5446b
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-11-03 22:43:02 +0000

    4.1.2. Глупый бот [#327521] Правильно работает только последний тест

---
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        assertThat(DummyBot.answer("Привет, Бот"), is("Привет, умник."));
+    }
+
+    @Test
+    public void whenByeBot() {
+        assertThat(DummyBot.answer("Пока."), is("До скорой встречи."));
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        assertThat(DummyBot.answer("Что ты думаешь о j4j?"), is("Это ставит меня в тупик. Задайте другой вопрос."));
+    }
+
+}
