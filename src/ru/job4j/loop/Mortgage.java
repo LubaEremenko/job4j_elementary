9e451b7dea1a0ffbc685a178c88201d29900bbe8
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-11-15 19:35:14 +0000

    5.7. Ипотека [#327622] - Run

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,16 +4,16 @@ public class Mortgage {
     public static int data(int amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = amount * percent / 100 - salary;
-            year++;
-            }
+            amount = (int) (amount + amount * percent / 100 - salary);
+            year = year + 1;
+        }
         return year;
 
     }
 
     public static void main(String[] arg) {
         double year = Mortgage.data(100, 120, 50);
-        System.out.println("На выплату кредита потребуется " + year + " года/лет.");
+        System.out.println("На выплату кредита потребуется " + year + " год(a).");
 
     }
 
