9997e55b4e7fc10845f6087393cf09c55f14602c
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-11-18 18:45:09 +0000

    6.0.2. Заполнение массива. [#327551]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,6 +6,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Иван Иванов, ";
+        names[1] = "Петр Петров, ";
+        names[2] = "Василий Пупкин, ";
+        names[3] = "Исидор Залупкин.";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
 
     }
 }
