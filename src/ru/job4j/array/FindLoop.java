d0fb4c94fabca434c21c220cf88ccad565683b03
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-11-21 20:10:21 +0000

    6.1. Классический поиск перебором. [#327537] Выдает неправильный ответ

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,26 +1,23 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public static int indexOf(int[] data, int el) {
-                int[] data = new int[5];
-                int rsl = -1;
+        public static int indexOf(int[] data, int el) {
+        int rsl = -1;
         for (int index = 0; index < data.length; index++) {
-            if(data[index] == el) {
+            if (data[index] == el) {
                 rsl = index;
                 break;
             }
             
         }
         return rsl;
-        System.out.println(data(rsl));
-        FindLoop.indexOf(5, 3);
+
     }
 
     public static void main(String[] args) {
-       int rsl;
-        int[] data = new int[] {6, 6, 8, 9, 3};
-        FindLoop.indexOf(5, 3);
-        System.out.println(data(rsl));
+           int[] data = new int[] {5, 1, 4, 6, 8};
+           int rsl = FindLoop.indexOf(new int[]{5}, 4);
+           System.out.println(rsl);
 
     }
 }
