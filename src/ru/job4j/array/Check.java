7bed31b09a9293331f7d1776fc0ba206a543a23b
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-11-24 19:01:58 +0000

    6.3. Массив заполнен true или false [#327540] исправила !=, убрала else

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -6,10 +6,9 @@ public class Check {
 
         boolean result = true;
         for (int index = 0; index < data.length - 1; index++) {
-            if (data[index]  == data[index + 1]) {
-             result = true;
-            } else {
+            if (data[index] != data[index + 1]) {
                 result = false;
+            break;
             }
 
         }
