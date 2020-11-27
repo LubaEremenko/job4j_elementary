4bdd9bacb6f448bd93083ee7cc79bbfd0a2e5fe3
Liubov Eremenko <eremenko.luba79@gmail.com>
2020-11-27 12:17:30 +0000

    6.5.3. Сортировка выборкой. [#327545] Какой-то странный вывод. Кажется он не сортирует совсем.

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        for (int index = 0; index <= data.length - 1; index++) {
+        for (int index = 0; index <= data.length-1; index++) {
             int min = MinDiapason.findMin(data, index, data.length - 1);
             int indexOfMin = FindLoop.indexOf(data, min, index, data.length - 1);
             int temp = data[min];
