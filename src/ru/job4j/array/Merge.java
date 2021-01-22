824a21709ddea56e0d6af23cb8b21edd32022806
Liubov Eremenko <eremenko.luba79@gmail.com>
2021-01-22 14:12:35 +0000

    3. Объединить два отсортированных массива 2 попытка

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,24 +1,47 @@
 package ru.job4j.array;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Merge {
-    public static int[] merge(int[] left, int [] right){
-        int[] rsl = new int[left.length + right.length];
-        for (int i = 0; i < left.length + right.length; i++) {
-            if (left[i] < right[i]) {
-                rsl[i] = left[i];
-            } else {
-                rsl[i] = right[i];
-            }
+    public static int[] merge(int[] left, int[] right) {
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        int[] rsl = new int[0];
+
+
+        while (i < left.length) {
+            rsl[k] = left[i];
+            i++;
+            k++;
 
         }
+
+        while (j < right.length) {
+            rsl[k] = right[j];
+            j++;
+            k++;
+
+        }
+        Arrays.sort(rsl);
         return rsl;
     }
 
-
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
-        int[] left = {5, 6, 2};
-        int[] right = {9, 3, 5};
-        int[] rsl = merge(int[] right, int[]right);
-        System.out.println(rsl);
+        int left[] = {5, 6, 2};
+        int right[] = {9, 3, 5};
+        int rsl[] = new int[left.length + right.length];
+
+        merge(left, right);
+
+        for(int i = 0; i < left.length + right.length; i++) {
+            System.out.println(rsl[i]);
+        }
+
+    }
 
 }
